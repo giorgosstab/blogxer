41717e9bcd5b44fde1cc6b47d186532363061ad9
giorgosstab <tsaxre1991@hotmail.com>
2019-10-18 12:21:44 +0300

    Add migration files

---
+++ application/migrations/20191017093128_create_tags_table.php
@@ -0,0 +1,46 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_create_tags_table extends CI_Migration {
+
+	/**
+	 * Name of the table to be used in this migration!
+	 *
+	 * @var string
+	 */
+	protected $_table_name = "tags";
+
+	public function up()
+	{
+		$this->dbforge->add_field(array(
+				'id' => array(
+					'type' => 'INTEGER',
+					'auto_increment' => TRUE,
+				),
+				'title' => array(
+					'type' => 'VARCHAR',
+					'constraint' => '255',
+					'unique' => TRUE,
+				),
+				'slug' => array(
+					'type' => 'VARCHAR',
+					'constraint' => '255',
+					'unique' => TRUE,
+				),
+			)
+		);
+		$this->dbforge->add_field("`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
+		$this->dbforge->add_field("`updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table($this->_table_name, TRUE);
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table($this->_table_name, TRUE);
+	}
+
+}
+
+?>
--- application/migrations/20191017093842_create_post_tag_table.php
+++ application/migrations/20191017093842_create_post_tag_table.php
@@ -2,14 +2,14 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_create_posts_table extends CI_Migration {
+class Migration_create_post_tag_table extends CI_Migration {
 
 	/**
 	 * Name of the table to be used in this migration!
 	 *
 	 * @var string
 	 */
-	protected $_table_name = "posts";
+	protected $_table_name = "post_tag";
 
 	public function up()
 	{
@@ -18,55 +18,38 @@ class Migration_create_posts_table extends CI_Migration {
 					'type' => 'INTEGER',
 					'auto_increment' => TRUE,
 				),
-				'category_id' => array(
+				'post_id' => array(
 					'type' => 'INTEGER',
 					'unsigned' => TRUE,
 					'default' => NULL,
 				),
-				'title' => array(
-					'type' => 'VARCHAR',
-					'constraint' => '255',
-				),
-				'slug' => array(
-					'type' => 'VARCHAR',
-					'constraint' => '255',
-					'unique' => TRUE,
-				),
-				'excerpt' => array(
-					'type' => 'TEXT',
-				),
-				'body' => array(
-					'type' => 'LONGTEXT',
-					'null' => NULL,
-				),
-				'on_top' => array(
-					'type' => 'TINYINT',
-					'default' => 0,
-				),
-				'hot' => array(
-					'type' => 'TINYINT',
-					'default' => 0,
-				),
-				'image' => array(
-					'type' => 'VARCHAR',
-					'constraint' => '255',
-					'null' => NULL,
+				'tag_id' => array(
+					'type' => 'INTEGER',
+					'unsigned' => TRUE,
+					'default' => NULL,
 				),
 			)
 		);
 		$this->dbforge->add_field("`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
 		$this->dbforge->add_field("`updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
-
 		$this->dbforge->add_key('id', TRUE);
-		// $this->dbforge->add_field('CONSTRAINT FOREIGN KEY (category_id) REFERENCES categories(id) ON UPDATE CASCADE ON DELETE SET NULL');
+
 		$this->dbforge->add_foreign_key(array(
-				'field' => 'category_id',
-				'foreign_table' => 'categories',
+				'field' => 'post_id',
+				'foreign_table' => 'posts',
 				'foreign_field' => 'id',
 				'delete' => 'SET NULL',
 				'update' => 'CASCADE',
 			)
 		);
+		$this->dbforge->add_foreign_key(array(
+			'field' => 'tag_id',
+			'foreign_table' => 'tags',
+			'foreign_field' => 'id',
+			'delete' => 'SET NULL',
+			'update' => 'CASCADE',
+		)
+	);
 		$this->dbforge->create_table($this->_table_name, TRUE);
 	}
 

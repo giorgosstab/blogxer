c9ee07c4441d537e90e062958a46dbe7dae84aa7
giorgosstab <tsaxre1991@hotmail.com>
2019-10-18 12:24:20 +0300

    Get data for home page

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -16,7 +16,12 @@ class Home extends Public_Controller {
      */
 
 	public function index() {
-		$data['posts'] = $this->post_model->get_posts();
+		$data['posts'] = $this->post_model->get_all_posts_with_tag();
+		$data['first_post'] = $this->post_model->getLatestPost();
+		// $data['posts'] = $this->post_model->get_posts();
+		$data['posts_ontop'] = $this->post_model->get_posts_ontop();
+		$data['posts_hot'] = $this->post_model->get_posts_hot();
+		$data['categories'] = $this->category_model->get_categories();
 		$this->render('public/home/index',$data);
 	}
 }
